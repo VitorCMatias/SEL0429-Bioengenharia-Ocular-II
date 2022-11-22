e359f59e692065012139ea52b69e0180d90d3f69
Vitor <vitorcosta.matias@usp.br>
2022-11-22 12:09:35 -0300

    Código de fluxo do display

---
+++ display.cpp
@@ -0,0 +1,58 @@
+ /************ Variáveis da tela ARMAÇÕES ******/
+
+const int armacao_ir = 1;      
+const int armacao_voltar = 1;  
+
+const int armacao_quadrado = 1;      
+const int armacao_arredondado = 1;      
+const int armacao_redondo = 1;      
+const int armacao_mascara = 1;      
+const int armacao_mascaradupla = 1;      
+const int armacao_alok = 1;   
+
+ /************ Variáveis da tela MATERIAIS ******/
+
+ const int material_ir = 1;      
+const int material_voltar = 1; 
+
+const int material_plastico = 1;      
+const int material_metal = 1;      
+const int material_madeira = 1;      
+const int material_borracha = 1;
+
+/************ Variáveis da tela MTERIAL HASTE ******/
+const int material_haste_ir = 1;      
+const int material_haste_voltar = 1; 
+
+const int material_haste_plastico = 1;      
+const int material_haste_metal = 1;      
+const int material_haste_madeira = 1;      
+const int material_haste_borracha = 1;
+
+ /************ Variáveis da tela HASTE ******/
+
+ const int haste_ir = 1;      
+const int haste_voltar = 1; 
+
+const int haste_fina = 1;      
+const int haste_media = 1;      
+const int haste_larga = 1;      
+
+ /************ Variáveis da tela PONTE ******/
+
+ const int ponte_ir = 1;      
+const int ponte_voltar = 1; 
+
+const int ponte_fina = 1;      
+const int ponte_media = 1;      
+const int ponte_larga = 1;  
+
+/************ Variáveis da tela MTERIAL PONTE ******/
+const int material_ponte_ir = 1;      
+const int material_ponte_voltar = 1; 
+
+const int material_ponte_plastico = 1;      
+const int material_ponte_metal = 1;      
+const int material_ponte_madeira = 1;      
+const int material_ponte_borracha = 1;
+
